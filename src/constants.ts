2cda6b86745e5c030df60d666584e847832f036f
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-01-18 18:36:54 -0500

    add menu

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,577 @@
+export const twitter = "@casa_realmexicanarestaurant";
 
-export const twitter = "@casa_realmexicanarestaurant"
 export const defaultMeta = {
   name: "Casa Real Mexican Restaurant",
   title: "Casa Real Mexican Restaurant",
   description: "Authentic Mexican food in the heart of the North Shore",
   image: "/images/og-image.jpg",
   url: "https://casa-real-mexican-restaurant.vercel.app/",
-}
+};
+
+export const menu = [
+  {
+    category: "Appetizers",
+    href: "#appetizers",
+    items: [
+      {
+        name: "Guacamole dip",
+        price: 550,
+      },
+      {
+        name: "Cheese dip",
+        price: 550,
+      },
+      {
+        name: "Bean dip",
+        price: 599,
+      },
+      {
+        name: "Queso fundido",
+        price: 825,
+      },
+      {
+        name: "Fiesta fries",
+        price: {
+          chicken: 1299,
+          steak: 1399,
+        },
+      },
+      {
+        name: "Quesadilla casa real",
+        price: {
+          chicken: 1275,
+          steak: 1425,
+          pastor: 1425,
+          shrimp: 1525,
+        },
+      },
+      {
+        name: "Appetizer casa real",
+        price: 1325,
+      },
+      {
+        name: "Jalapeño poppers",
+        price: 875,
+      },
+      {
+        name: "Wings",
+        price: 1499,
+      },
+      {
+        name: "Street corn",
+        price: 899,
+      },
+    ],
+  },
+  {
+    category: "Salads",
+    href: "#salads",
+    items: [
+      {
+        name: "Casa real salad",
+        price: {
+          chicken: 1175,
+          steak: 1299,
+          shrimp: 1499,
+        },
+      },
+      {
+        name: "Casa real bowl",
+        price: {
+          chicken: 1250,
+          ground_beef: 1250,
+          grilled_chicken: 1450,
+          steak: 1600,
+          pastor: 1600,
+          shrimp: 1725,
+        },
+      },
+      {
+        name: "Chipotle bowl",
+        price: {
+          grilled_chicken: 1450,
+          steak: 1600,
+          pastor: 1600,
+          shrimp: 1725,
+        },
+      },
+    ],
+  },
+  {
+    category: "Nachos",
+    href: "#nachos",
+    items: [
+      {
+        name: "Nachos supremos",
+        price: 1450,
+      },
+      {
+        name: "Nachos",
+        price: {
+          cheese: 850,
+          beans: 850,
+          grilled_chicken: 1275,
+          shrimp_nachos: 1550,
+          chicken: 975,
+          ground_beef: 975,
+          steak: 1225,
+        },
+      },
+    ],
+  },
+  {
+    category: "House Specialties",
+    href: "#house-specialties",
+    items: [
+      {
+        name: "Dinner special",
+        price: 1925,
+      },
+      {
+        name: "Taquitos mexicanos",
+        price: 1225,
+      },
+      {
+        name: "Taco salad",
+        price: 1175,
+      },
+      {
+        name: "Taco salad fajita",
+        price: {
+          chicken: 1275,
+          steak: 1350,
+          shrimp: 1499,
+        },
+      },
+      {
+        name: "Chimichanga",
+        price: 1299,
+      },
+      {
+        name: "Carnitas",
+        price: 1599,
+      },
+      {
+        name: "Nachos fajitas",
+        price: {
+          chicken: 1299,
+          steak: 1450,
+          shrimp: 1699,
+        },
+      },
+      {
+        name: "Molcajete",
+        price: 2125,
+      },
+      {
+        name: "Lalo's special",
+        price: 1650,
+      },
+    ],
+  },
+  {
+    category: "Quesadillas",
+    href: "#quesadillas",
+    items: [
+      {
+        name: "Quesadillas mexicanas",
+        price: 1250,
+      },
+      {
+        name: "Quesadillas de fajita",
+        price: {
+          chicken: 1450,
+          steak: 1575,
+          pastor: 1575,
+          shrimp: 1650,
+        },
+      },
+      {
+        name: "La gringa",
+        price: 1625,
+      },
+      {
+        name: "Quesadilla Popeye's",
+        price: 1575,
+      },
+      {
+        name: "Quesabirrias",
+        price: 1599,
+      },
+      {
+        name: "Quesadilla la chicken tips",
+        price: 1325,
+      },
+    ],
+  },
+  {
+    category: "Enchiladas",
+    href: "#enchiladas",
+    items: [
+      {
+        name: "Enchiladas supremas",
+        price: 1425,
+      },
+      {
+        name: "Enchiladas rancheras",
+        price: 1425,
+      },
+      {
+        name: "Enchiladas popeye's",
+        price: 1425,
+      },
+      {
+        name: "Enchiladas acapulco",
+        price: 1599,
+      },
+      {
+        name: "Yolanda's",
+        price: 1399,
+      },
+    ],
+  },
+  {
+    category: "Tacos",
+    href: "#tacos",
+    items: [
+      {
+        name: "Avocado tacos",
+        price: 1275,
+      },
+      {
+        name: "Carne asada tacos",
+        price: 1425,
+      },
+      {
+        name: "Carnitas tacos",
+        price: 1425,
+      },
+      {
+        name: "Fish tacos",
+        price: 1575,
+      },
+      {
+        name: "Tacos al pastor",
+        price: 1475,
+      },
+      {
+        name: "Tacos supremos",
+        price: 1225,
+      },
+      {
+        name: "Tacos de birria",
+        price: 1499,
+      },
+    ],
+  },
+  {
+    category: "Burritos",
+    href: "#burritos",
+    items: [
+      {
+        name: "Burritos mexicanos",
+        price: 1325,
+      },
+      {
+        name: "Burritos típicos",
+        price: 1250,
+      },
+      {
+        name: "Burritos típicos",
+        price: 1250,
+      },
+      {
+        name: "Burrito chipotle",
+        price: {
+          veggies: 1525,
+          chicken: 1499,
+          steak: 1650,
+          pastor: 1650,
+          shrimp: 1750,
+        },
+      },
+      {
+        name: "Burrito california",
+        price: {
+          veggies: 1525,
+          chicken: 1499,
+          steak: 1650,
+          pastor: 1650,
+          shrimp: 1750,
+        },
+      },
+      {
+        name: "Burrito casa real",
+        price: {
+          veggies: 1525,
+          chicken: 1499,
+          steak: 1650,
+          pastor: 1650,
+          shrimp: 1750,
+        },
+      },
+      {
+        name: "Burrito popeye's",
+        price: 1325,
+      },
+      {
+        name: "Skillet burrito",
+        price: {
+          chicken: 1525,
+          steak: 1725,
+          pastor: 1725,
+          carnitas: 1725,
+        },
+      },
+    ],
+  },
+  {
+    category: "Steak",
+    href: "#steak",
+    items: [
+      {
+        name: "Carne asada",
+        price: 1699,
+      },
+      {
+        name: "Steak a la mexicana",
+        price: 1799,
+      },
+      {
+        name: "Steak casa real",
+        price: 2050,
+      },
+      {
+        name: "Steak oaxaca",
+        price: 1899,
+      },
+      {
+        name: "Steak maya",
+        price: 1850,
+      },
+    ],
+  },
+  {
+    category: "Chicken",
+    href: "#chicken",
+    items: [
+      {
+        name: "El tapatío",
+        price: 1599,
+      },
+      {
+        name: "Pollo loco",
+        price: 1499,
+      },
+      {
+        name: "Pollo encremado",
+        price: 1575,
+      },
+      {
+        name: "Pollo maya",
+        price: 1599,
+      },
+      {
+        name: "Ángel",
+        price: 1299,
+      },
+    ],
+  },
+  {
+    category: "Fajitas",
+    href: "#fajitas",
+    items: [
+      {
+        name: "Fajitas",
+        price: {
+          chicken: 1650,
+          steak: 1750,
+          mix: 1825,
+          texana: 1950,
+          special: 1999,
+          hawaiana: 1699,
+          shrimp: 1999,
+          pork: 1925,
+          del_mar: 2099,
+          veggies: 1499,
+        },
+      },
+    ],
+  },
+  {
+    category: "Seafood",
+    href: "#seafood",
+    items: [
+      {
+        name: "Coctel de camarón",
+        price: 1800,
+      },
+      {
+        name: "Veracruz plate",
+        price: 1825,
+      },
+      {
+        name: "Lago especial",
+        price: 1850,
+      },
+      {
+        name: "Camarón yucatán",
+        price: 1825,
+      },
+      {
+        name: "Rice with chicken or shrimp",
+        price: {
+          chicken: 1225,
+          shrimp: 1725,
+        },
+      },
+      {
+        name: "Brochetas",
+        price: 1999,
+      },
+      {
+        name: "Al pastor brochetas",
+        price: 1899,
+      },
+    ],
+  },
+  {
+    category: "Lunch Specials",
+    href: "#lunch-specials",
+    items: [
+      {
+        name: "Speed gonzales",
+        price: 850,
+      },
+      {
+        name: "Lunch chimichanga",
+        price: {
+          beef: 975,
+          chicken: 975,
+          shrimp: 1199,
+        },
+      },
+      {
+        name: "Burrito mexicano",
+        price: 850,
+      },
+      {
+        name: "Huevos rancheros",
+        price: 899,
+      },
+      {
+        name: "Huevos con chorizo",
+        price: 925,
+      },
+      {
+        name: "El rancho",
+        price: 1050,
+      },
+      {
+        name: "Burrito especial",
+        price: 799,
+      },
+      {
+        name: "Burrito especial",
+        price: 799,
+      },
+      {
+        name: "Burrito and enchilada",
+        price: 899,
+      },
+      {
+        name: "Burrito verde",
+        price: 950,
+      },
+      {
+        name: "Quesadilla especial",
+        price: 850,
+      },
+      {
+        name: "El burrito alteño",
+        price: 850,
+      },
+      {
+        name: "Fajita lunch special",
+        price: {
+          chicken: 999,
+          steak: 1175,
+          pork: 1175,
+          shrimp: 1275,
+        },
+      },
+      {
+        name: "Fajita lunch special",
+        price: {
+          chicken: 1025,
+          steak: 1150,
+          shrimp: 1350,
+        },
+      },
+      {
+        name: "Flautas",
+        price: 925,
+      },
+    ],
+  },
+  {
+    category: "Desserts",
+    href: "#desserts",
+    items: [
+      {
+        name: "Combo casa real",
+        price: 1299,
+      },
+      {
+        name: "Fried ice cream",
+        price: 825,
+      },
+      {
+        name: "Fried cheesecake",
+        price: 799,
+      },
+      {
+        name: "Churros",
+        price: 675,
+      },
+      {
+        name: "Sopapilla",
+        price: 599,
+      },
+      {
+        name: "Flan",
+        price: 599,
+      },
+    ],
+  },
+  {
+    category: "Kids Menu",
+    href: "#kids-menu",
+    items: [
+      {
+        name: "Macaroni and cheese",
+        price: 850,
+      },
+      {
+        name: "Chicken tenders",
+        price: 850,
+      },
+      {
+        name: "Cheeseburger",
+        price: 850,
+      },
+      {
+        name: "Enchilada",
+        price: 850,
+      },
+      {
+        name: "Taco",
+        price: 850,
+      },
+      {
+        name: "Burrito",
+        price: 850,
+      },
+    ],
+  },
+];
