719c657914b1a2bd5e22977d4fed506e09cecf0b
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-01-25 22:49:15 -0500

    Update .gitignore, robots.txt, package.json, README.md, and astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://superior-sphere-khaki.vercel.app/",
+  site: "https://website-nine-pied.vercel.app/",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [tailwind(), sitemap()],
 });
