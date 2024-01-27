701961f89f072f8ac94285356736af343c0cb234
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-01-27 13:35:33 -0500

    Update site URL and sitemap URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://website-nine-pied.vercel.app/",
+  site: "https://www.casarealvt.com",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
